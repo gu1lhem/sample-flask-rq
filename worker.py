2090735b37c7f56d507eba56a2f2c19eec118cc4
gu1lhem <38001093@parisnanterre.fr>
2021-12-19 17:00:12 +0100

    Now Redis imports from the env file

--- worker.py
+++ worker.py
@@ -1,11 +1,14 @@
-import os
+from os import environ
+
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
 
 import redis
 from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = environ.get('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
